ad41802690bfa2c646ec5e0d76fd4783e73c2968
Olena Korenieva <olena.korenieva@gmail.com>
2023-02-07 10:30:13 +0100

    update search parameters for fetch function

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-export async function fetchImages(value, page) {
-  const BASE_URL = 'https://pixabay.com/api/?';
+export async function fetchImages(q, page) {
+  const BASE_URL = 'https://pixabay.com/api/';
   const searchParams = new URLSearchParams({
     key: '30860486-2bc10cd6f698c5db7e18c3090',
-    q: `${value}`,
+    q,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     per_page: 40,
-    page: `${page}`,
+    page,
   });
 
-  return await axios.get(`${BASE_URL}${searchParams}`).then(response => {
+  return await axios.get(`${BASE_URL}?${searchParams}`).then(response => {
     return response.data;
   });
 }
